40f41aa7273231bbb274fc0af464d260f81f6fb5
ikeura <ikeryo1182@gmail.com>
2019-07-22 00:20:12 +0900

    add comment function interface

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 
+export interface CommentType {
+    name: string;
+    message: string;
+}
+
 interface Props {
-    isOpen: boolean;
+    comment: CommentType
+    isOpen: boolean | null;
     onClick?: () => void;
+    onSubmit?: () => void;
+    onChangeComment?: (comment: CommentType) => void;
 }
 
 const Comment: React.FC<Props> = (props) => {
-    const { isOpen, onClick } = props;
+    const { comment, isOpen, onClick, onSubmit, onChangeComment } = props;
 
     const handleClick = () => {
         if (onClick) onClick();
+    }
+
+    const handleChangeComment = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.currentTarget
+
+        if (onChangeComment) onChangeComment(Object.assign({}, comment, { [name]: value }))
+    }
+
+    const handleSubmit = () => {
+        if(onSubmit) onSubmit();
     }
 
     return (
@@ -17,11 +35,21 @@ const Comment: React.FC<Props> = (props) => {
             {isOpen ?
                 (<div className="input-wrapper">
                     <div className="input">
+                        <p className="input-message" >Please make a short comment about me or this page 😄</p>
+                        <form id="form" onSubmit={handleSubmit} >
+                            <p className="label-wrapper"><label>Name</label></p>
+                            <p><input required name="name" onChange={handleChangeComment} defaultValue={comment.name}></input></p>
+                            <p className="label-wrapper"><label>Message</label></p>
+                            <p><textarea required name="message" onChange={handleChangeComment} defaultValue={comment.message}></textarea></p>
+                        </form>
+                        <div className="button-wrapper">
+                            <button form="form" className="submit-button">OK</button>
+                        </div>
                     </div>
                 </div>)
                 : null}
             <div onClick={handleClick} className="comment-button">
-                <span className="icon">{isOpen ? <img width={14} src="/static/assets/close.png"/> : <img width={14} src="/static/assets/open.png"/>}</span>
+                <span className="icon">{isOpen ? <img width={14} src="/static/assets/close.png" /> : <img width={14} src="/static/assets/open.png" />}</span>
             </div>
             <style jsx>{`
                 .global-wrapper {
@@ -39,13 +67,60 @@ const Comment: React.FC<Props> = (props) => {
                     animation: img-wrap 0.5s cubic-bezier(.4, 0, .2, 1) forwards;
                     position:relative;
                     right:165px;
-                    bottom:310px;
+                    bottom:390px;
                     background-color: blue;
-                    width:200px;
-                    height:300px;
+                    width:160px;
+                    height:340px;
                     transition:1s;
                     border-radius:5px;
+                    padding:20px;
                 }
+                p {
+                    margin:3px 0px;
+                }
+                .label-wrapper {
+                    height:18px;
+                }
+                label {
+                    font-size:12px;
+                    color:white;
+                }
+                input,textarea {
+                    outline: 0;
+                }
+                input {
+                    width:150px;
+                }
+                textarea {
+                    width:150px;
+                    max-width:150px;
+                    height:160px;
+                    max-height:160px;
+                }
+                .input-message {
+                    line-height:18px;
+                    color:white;
+                    margin-bottom:20px;
+                }
+                .button-wrapper {
+                    display:flex;
+                    justify-content: flex-end;
+                }
+                .submit-button {
+                    cursor:pointer;
+                    border-radius:5px;
+                    width:50px;
+                    height:30px;
+                    outline:none;
+                    color:white;
+                    border:none;
+                    background-color: rgba(0,0,0,0);
+                }
+                .submit-button:hover {
+                    transition:0.3s;
+                    background-color: rgba(255,255,255, 0.3);
+                }
+
                 @keyframes img-wrap {
                     0% {
                       opacity:0;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,54 @@
 import { NextPage } from 'next'
 import React from "react"
-import Comment from '../components/Comment';
+import Comment, { CommentType } from '../components/Comment';
 
 interface Props {
 }
 
 const useIsOpenCommentState = () => {
-  const [isOpenComment, setIsOpenComment] = React.useState(false)
+  // TODO: boolean | null は気持ち悪いから enum 使う
+  const [isOpenComment, setIsOpenComment] = React.useState<boolean | null>(false)
 
-  return { isOpenComment, setIsOpenComment}
+  return { isOpenComment, setIsOpenComment }
+}
+
+const useCommentState = () => {
+  const [comment, setComment] = React.useState({ name: "", message: "" })
+
+  return { comment, setComment }
 }
 
 const IndexPage: NextPage<Props> = () => {
   const { isOpenComment, setIsOpenComment } = useIsOpenCommentState();
+  const { comment, setComment } = useCommentState();
 
   const handleClick = () => {
     setIsOpenComment(!isOpenComment)
   }
 
+  const handleChangeComment = (comment: CommentType) => {
+    setComment(comment);
+  }
+
+  const handleSubmit = () => {
+    setIsOpenComment(null);
+    alert("Comment function is in development 😓")
+  }
+
   return (
     <main>
-      <Comment isOpen={isOpenComment} onClick={handleClick} />
+      <div>
+        Introduction Page
+      </div>
+      {isOpenComment !== null ? (
+        <Comment comment={comment} isOpen={isOpenComment} onClick={handleClick} onChangeComment={handleChangeComment} onSubmit={handleSubmit} />
+      ) : null}
       <style jsx>{`
         * {
           user-select: none;
+        }
+        main {
+          font-family: Avenir,Helvetica,Arial,sans-serif;
         }
       `}</style>
     </main>
